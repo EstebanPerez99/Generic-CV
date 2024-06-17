cc99aa9868a900daac15b5739ca94fc432965a86
EstebanPerez99 <epm1520@gmail.com>
2024-06-17 00:24:39 -0600

    analytics

--- src/GoogleAnalytics.tsx
+++ src/GoogleAnalytics.tsx
@@ -4,21 +4,23 @@ import Script from "next/script";
 const GoogleAnalytics = () => {
 	return (
 		<>
-			<Script
-				strategy='lazyOnload'
+			{/* Google Analytics Measurement ID*/}
+			<script
+				async
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
 			/>
-
-			<Script id='' strategy='lazyOnload'>
-				{`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-			</Script>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname
+                });
+              `,
+				}}
+			/>
 		</>
 	);
 };
